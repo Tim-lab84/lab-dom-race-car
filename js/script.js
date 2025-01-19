a96c931381ad33881f88c7583fd495ea807c5732
Timothy Williams <TimothyCalvinWilliams@gmail.com>
2025-01-19 16:12:32 +0100

    huh

--- js/script.js
+++ js/script.js
@@ -1,12 +1,90 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+
   function startGame() {
     console.log("start game");
+    game = new Game();
+
+    game.start();
   }
+
+  // The function that reloads the page to start a new game
+  function restartGame() {
+    location.reload();
+  }
+
+  // Function that handles keydown event
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1.5;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1.5;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  // Function that handles keyup event
+  function handleKeyup(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the released key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Reset player's directionX and directionY when key is released
+      switch (key) {
+        case "ArrowLeft":
+        case "ArrowRight":
+          game.player.directionX = 0;
+          break;
+        case "ArrowUp":
+        case "ArrowDown":
+          game.player.directionY = 0;
+          break;
+      }
+    }
+  }
+
+  // Add the handleKeydown and handleKeyup functions as event listeners
+  window.addEventListener("keydown", handleKeydown);
+  window.addEventListener("keyup", handleKeyup);
 };
